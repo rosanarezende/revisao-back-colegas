92a7287e9bd942fa6a9fce47274253aa20cec533
Rosana Rezende <rezende_rosana@hotmail.com>
2020-05-26 10:38:20 -0300

    testando o servidor

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import express from "express";
+import express, {Request, Response} from "express";
 import { AddressInfo } from "net";
 import dotenv from "dotenv";
 dotenv.config();
@@ -6,6 +6,12 @@ dotenv.config();
 const app = express();
 app.use(express.json());
 
+// //testando o servidor
+// app.get("/", (req: Request, res: Response) => {
+//     res.status(200).send({
+//         message: "success"
+//     })
+// })
 
 const server = app.listen(process.env.PORT || 3002, () => {
     if (server) {
